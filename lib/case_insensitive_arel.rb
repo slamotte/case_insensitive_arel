9c89bce77cd66ad5861194d31a078f9a53c8be47
Steve Lamotte <steve@lexor.ca>
2011-03-18 00:18:32 -0500

    Refactored method aliasing

--- lib/case_insensitive_arel.rb
+++ lib/case_insensitive_arel.rb
@@ -4,20 +4,16 @@ require 'active_support/core_ext/module/aliasing'
 module Arel
   module Visitors
     class ToSql
-      def visit_Arel_Attributes_Attribute_with_case_insensitive(o)
-        Arel::CaseInsensitive.convert_value visit_Arel_Attributes_Attribute_without_case_insensitive(o), :convert_attribute
+      [
+          ['Arel_Attributes_Attribute', :convert_attribute],
+          ['Arel_Attributes_String', :convert_attribute],
+          ['String', :convert_string]
+      ].each do |type_name, convert_method|
+        define_method "visit_#{type_name}_with_case_insensitive" do |o|
+          Arel::CaseInsensitive.convert_value send("visit_#{type_name}_without_case_insensitive", o), convert_method
+        end
+        alias_method_chain "visit_#{type_name}", :case_insensitive
       end
-      alias_method_chain :visit_Arel_Attributes_Attribute, :case_insensitive
-
-      def visit_Arel_Attributes_String_with_case_insensitive(o)
-        Arel::CaseInsensitive.convert_value visit_Arel_Attributes_String_without_case_insensitive(o), :convert_attribute
-      end
-      alias_method_chain :visit_Arel_Attributes_String, :case_insensitive
-
-      def visit_String_with_case_insensitive(o)
-        Arel::CaseInsensitive.convert_value visit_String_without_case_insensitive(o), :convert_string
-      end
-      alias_method_chain :visit_String, :case_insensitive
     end
   end
 
@@ -30,6 +26,7 @@ module Arel
   end
 end
 
+# Set the default values (for Oracle)
 Arel::CaseInsensitive.case_insensitive = true
 Arel::CaseInsensitive.convert_attribute = Proc.new { |val| "UPPER(#{val})" }
 Arel::CaseInsensitive.convert_string = Proc.new{ |val| val.upcase }
